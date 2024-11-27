7da5a4605c66da81ef36637853efa6f4d21d88c9
Levy <golvwei@gmail.com>
2024-11-27 15:26:49 +0800

    feat: export server

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Express } from 'express';
+
+const server: Express = express();
 
 async function bootstrap() {
   const logger = new Logger();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   const port = parseInt(process.env.PORT, 10) || 3000;
   const env = process.env.NODE_ENV || 'development';
+
   app.enableCors();
   app.setGlobalPrefix('v1');
   app.useGlobalPipes(
@@ -19,10 +25,18 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(port);
-  logger.log(`=================================`);
-  logger.log(`======= ENV: ${env} =======`);
-  logger.log(`🚀 Application listening on port ${port}`);
-  logger.log(`=================================`);
+
+  if (process.env.NODE_ENV !== 'production') {
+    await app.listen(port);
+    logger.log(`=================================`);
+    logger.log(`======= ENV: ${env} =======`);
+    logger.log(`🚀 Application listening on port ${port}`);
+    logger.log(`=================================`);
+  } else {
+    await app.init();
+  }
 }
+
 bootstrap();
+
+export default server;
